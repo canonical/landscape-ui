276a0458c047a6a8fdc44cdd9d9859023626002a
Yuriy Vasilyev <hello@yuriy.works>
2023-05-17 15:22:18 +0300

    Adjust error handling

--- src/hooks/useDebug.ts
+++ src/hooks/useDebug.ts
@@ -1,12 +1,24 @@
 import { IS_DEV_ENV } from "../constants";
 import useNotify from "./useNotify";
+import { AxiosError } from "axios";
 
 export default function useDebug() {
   const notify = useNotify();
 
-  return (error: any) => {
-    const message =
-      error?.response?.data.message ?? error?.message ?? "Unknown error";
+  return (error: unknown) => {
+    let message: string;
+
+    if (error instanceof AxiosError) {
+      message = error.message;
+
+      if (IS_DEV_ENV) {
+        console.error(error.response);
+      }
+    } else if (error instanceof Error) {
+      message = error.message;
+    } else {
+      message = "Unknown error";
+    }
 
     notify.error(message);
 
