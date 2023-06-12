cd670452884cf2c00337e542a86e4811c310fd5c
Yuriy Vasilyev <hello@yuriy.works>
2023-06-12 17:02:08 +0200

    Add redirect to /repositories

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,8 +1,14 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import DashboardTemplate from "../../templates/dashboard";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const DashboardPage: FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/repositories", { replace: true });
+  }, []);
+
   return (
     <DashboardTemplate>
       <Outlet />
