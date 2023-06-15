ed0410c8b74fcd93acb9de3f25ddb88365a6cac4
Yuriy Vasilyev <hello@yuriy.works>
2023-06-15 08:46:47 +0300

    Add ability to render difference between pulled pocket packages and pocket packages it was pulled from

--- src/pages/dashboard/repositories/PackageList.tsx
+++ src/pages/dashboard/repositories/PackageList.tsx
@@ -11,18 +11,29 @@ import {
   Pagination,
   Row,
 } from "@canonical/react-components";
-import { Package } from "../../../types/Package";
 import useDebug from "../../../hooks/useDebug";
+import classNames from "classnames";
+import useConfirm from "../../../hooks/useConfirm";
+import useSidePanel from "../../../hooks/useSidePanel";
+import EditPocketForm from "./EditPocketForm";
+import classes from "./PackageList.module.scss";
+
+interface FormattedPackage {
+  packageName: string;
+  packageVersion: string;
+  difference?: "update" | "delete" | "add";
+  newVersion?: string;
+}
 
 const paginationLimit = 15;
 
-interface PackageFormProps {
+interface PackageListProps {
   pocket: Pocket;
   distribution: Distribution;
   series: Series;
 }
 
-const PackageList: FC<PackageFormProps> = ({
+const PackageList: FC<PackageListProps> = ({
   distribution,
   pocket,
   series,
@@ -31,14 +42,20 @@ const PackageList: FC<PackageFormProps> = ({
   const [selectedPackages, setSelectedPackages] = useState<number[]>([]);
   const [checked, setChecked] = useState(false);
   const [indeterminate, setIndeterminate] = useState(false);
+  const [hasUpdatedOrDeletedPackages, setHasUpdatedOrDeletedPackages] =
+    useState(false);
 
   const debug = useDebug();
+  const { closeConfirmModal, confirmModal } = useConfirm();
+  const { setSidePanelContent } = useSidePanel();
 
   const {
     listPocketQuery,
+    diffPullPocketQuery,
     pullPackagesToPocketQuery,
     syncMirrorPocketQuery,
     removePackagesFromPocketQuery,
+    removePocketQuery,
   } = usePockets();
 
   const { data: pocketPackagesData, error: pocketPackagesListError } =
@@ -52,58 +69,173 @@ const PackageList: FC<PackageFormProps> = ({
     debug(pocketPackagesListError);
   }
 
-  const pocketPackages: Package[] = [];
+  const pocketPackages: FormattedPackage[] = [];
 
   if (pocketPackagesData) {
     for (const dataKey in pocketPackagesData.data) {
-      pocketPackages.push(...pocketPackagesData.data[dataKey]);
+      for (const [packageName, packageVersion] of pocketPackagesData.data[
+        dataKey
+      ]) {
+        pocketPackages.push({ packageName, packageVersion });
+      }
     }
   }
 
-  const checkBoxRef = useRef<HTMLInputElement>(null);
+  const { data: diffPullPocketData, error: diffPullPocketError } =
+    diffPullPocketQuery(
+      {
+        name: pocket.name,
+        series: series.name,
+        distribution: distribution.name,
+      },
+      {
+        enabled: "pull" === pocket.mode,
+      }
+    );
 
-  const packagesToShow = pocketPackages.filter(
+  if (diffPullPocketError) {
+    debug(diffPullPocketError);
+  }
+
+  const diffPullPocket: {
+    packageName: string;
+    newVersion: string;
+    difference: "update" | "delete" | "add";
+  }[] = [];
+
+  if (diffPullPocketData) {
+    for (const dataKey in diffPullPocketData.data) {
+      const diff = diffPullPocketData.data[dataKey];
+
+      if (diff.add) {
+        for (const addElement of diff.add) {
+          diffPullPocket.push({
+            packageName: addElement[0],
+            newVersion: addElement[1],
+            difference: "add",
+          });
+        }
+      }
+
+      if (diff.update) {
+        setHasUpdatedOrDeletedPackages(true);
+
+        for (const updateElement of diff.update) {
+          diffPullPocket.push({
+            packageName: updateElement[0],
+            newVersion: updateElement[2],
+            difference: "update",
+          });
+        }
+      }
+
+      if (diff.delete) {
+        setHasUpdatedOrDeletedPackages(true);
+
+        for (const deleteElement of diff.delete) {
+          diffPullPocket.push({
+            packageName: deleteElement[0],
+            difference: "delete",
+            newVersion: "",
+          });
+        }
+      }
+    }
+  }
+
+  const getSortedPackages = (): FormattedPackage[] => {
+    if (!hasUpdatedOrDeletedPackages) {
+      return pocketPackages;
+    }
+
+    pocketPackages.forEach((pocketPackage, index) => {
+      const alteredPackage = diffPullPocket.find(
+        ({ packageName }) => pocketPackage.packageName === packageName
+      );
+
+      if (alteredPackage) {
+        pocketPackages[index].difference = alteredPackage.difference;
+
+        if ("" !== alteredPackage.newVersion) {
+          pocketPackages[index].newVersion = alteredPackage.newVersion;
+        }
+      }
+    });
+
+    return pocketPackages.sort((a, b) => {
+      if (a.difference && !b.difference) {
+        return -1;
+      }
+      if (!a.difference && b.difference) {
+        return 1;
+      }
+      return 0;
+    });
+  };
+
+  const sortedPackages = getSortedPackages();
+
+  const packagesToShow = sortedPackages.filter(
     (_, index) =>
       index >= (currentPage - 1) * paginationLimit &&
       index < currentPage * paginationLimit
   );
 
-  const rows = packagesToShow.map(([packageName, packageVersion], index) => {
-    const firstColumnContent =
-      "upload" === pocket.mode ? (
-        <label className="p-checkbox--inline">
-          <input
-            type="checkbox"
-            checked={selectedPackages.includes(index)}
-            className="p-checkbox__input"
-            onChange={(event) => {
-              setSelectedPackages(
-                event.target.checked
-                  ? (prevState) => [...prevState, index]
-                  : (prevState) =>
-                      prevState.filter(
-                        (selectedPackage) => selectedPackage !== index
-                      )
-              );
-            }}
-          />
-          <span className="p-checkbox__label">{packageName}</span>
-        </label>
-      ) : (
-        packageName
-      );
+  const rows = packagesToShow.map(
+    ({ packageName, packageVersion, difference, newVersion }, index) => {
+      const firstColumnContent =
+        "upload" === pocket.mode ? (
+          <label className="p-checkbox--inline">
+            <input
+              type="checkbox"
+              checked={selectedPackages.includes(index)}
+              className="p-checkbox__input"
+              onChange={(event) => {
+                setSelectedPackages(
+                  event.target.checked
+                    ? (prevState) => [...prevState, index]
+                    : (prevState) =>
+                        prevState.filter(
+                          (selectedPackage) => selectedPackage !== index
+                        )
+                );
+              }}
+            />
+            <span className="p-checkbox__label">{packageName}</span>
+          </label>
+        ) : (
+          packageName
+        );
 
-    return {
-      columns: [
-        {
-          content: firstColumnContent,
-        },
-        {
-          content: packageVersion,
-        },
-      ],
-    };
-  });
+      const versionInfo =
+        "pull" === pocket.mode && difference ? (
+          <div className={classNames("p-tooltip--right", classes.withIcon)}>
+            <span>{packageVersion}</span>
+            <i className="p-icon--warning" />
+            <span className="p-tooltip__message" role="tooltip">
+              {"update" === difference
+                ? `Version differs\nfrom parent pocket.\nParent version: ${newVersion}`
+                : "Package deleted"}
+            </span>
+          </div>
+        ) : (
+          packageVersion
+        );
+
+      return {
+        columns: [
+          {
+            content: firstColumnContent,
+          },
+          {
+            content: versionInfo,
+          },
+        ],
+      };
+    }
+  );
+
+  const checkBoxRef = useRef<HTMLInputElement>(null);
 
   useLayoutEffect(() => {
     if (0 === rows.length) {
@@ -198,7 +330,7 @@ const PackageList: FC<PackageFormProps> = ({
   const handleRemovePackages = () => {
     const packages = packagesToShow
       .filter((_, index) => selectedPackages.includes(index))
-      .map(([packageName]) => packageName);
+      .map(({ packageName }) => packageName);
 
     removePackagesFromPocket({
       name: pocket.name,
@@ -208,45 +340,103 @@ const PackageList: FC<PackageFormProps> = ({
     });
   };
 
+  const handleEditPocket = () => {
+    setSidePanelContent(
+      `Edit ${pocket.name} pocket`,
+      <EditPocketForm
+        pocket={pocket}
+        distribution={distribution}
+        series={series}
+      />
+    );
+  };
+
+  const {
+    mutate: removePocket,
+    isLoading: isRemovingPocket,
+    error: removePocketError,
+  } = removePocketQuery;
+
+  if (removePocketError) {
+    debug(removePocketError);
+  }
+
+  const handleRemovePocket = () => {
+    confirmModal({
+      body: `Do you really want to delete ${pocket.name} pocket from ${series.name} series of ${distribution.name} distribution?`,
+      title: "Deleting pocket",
+      buttons: [
+        <Button
+          key={`delete-${pocket.name}-pocket`}
+          appearance="negative"
+          disabled={isRemovingPocket}
+          onClick={() => {
+            removePocket({
+              distribution: distribution.name,
+              series: series.name,
+              name: pocket.name,
+            });
+
+            closeConfirmModal();
+          }}
+        >
+          Delete
+        </Button>,
+      ],
+    });
+  };
+
   return (
     <>
       <Row className="u-no-padding--left u-no-padding--right ">
-        <Col size={"upload" === pocket.mode ? 7 : 6} className="is-bordered">
-          {"upload" !== pocket.mode && (
-            <Button
-              appearance="base"
-              small
-              dense
-              className="u-no-margin--right"
-              onClick={handleSync}
-              disabled={
-                ("mirror" === pocket.mode && isSynchronizingMirrorPocket) ||
-                ("pull" === pocket.mode && isPullingPackagesToPocket)
-              }
-            >
-              Sync
-            </Button>
-          )}
-          <Button appearance="base" small dense className="u-no-margin--right">
-            Edit
-          </Button>
-          <Button appearance="base" small dense className="u-no-margin--right">
-            Remove
-          </Button>
-          {"upload" === pocket.mode && (
-            <Button
-              appearance="base"
-              small
-              dense
-              className="u-no-margin--right"
-              disabled={
-                0 === selectedPackages.length || isRemovingPackagesFromPocket
-              }
-              onClick={handleRemovePackages}
-            >
-              Remove packages
-            </Button>
-          )}
+        <Col
+          size={"upload" === pocket.mode ? 8 : 6}
+          className={classNames("is-bordered")}
+        >
+          <div className="p-segmented-control">
+            <div className="p-segmented-control__list">
+              {"upload" !== pocket.mode && (
+                <Button
+                  dense
+                  className="p-segmented-control__button"
+                  onClick={handleSync}
+                  disabled={
+                    ("mirror" === pocket.mode && isSynchronizingMirrorPocket) ||
+                    ("pull" === pocket.mode && isPullingPackagesToPocket)
+                  }
+                >
+                  Sync
+                </Button>
+              )}
+              <Button
+                dense
+                className="p-segmented-control__button"
+                onClick={handleEditPocket}
+              >
+                Edit
+              </Button>
+              <Button
+                dense
+                className="p-segmented-control__button"
+                onClick={handleRemovePocket}
+              >
+                Remove
+              </Button>
+              {"upload" === pocket.mode && (
+                <Button
+                  dense
+                  className="p-segmented-control__button"
+                  disabled={
+                    0 === selectedPackages.length ||
+                    isRemovingPackagesFromPocket
+                  }
+                  onClick={handleRemovePackages}
+                >
+                  Remove packages
+                </Button>
+              )}
+            </div>
+          </div>
         </Col>
         {"upload" !== pocket.mode && (
           <Col size={6}>
@@ -263,6 +453,7 @@ const PackageList: FC<PackageFormProps> = ({
         headers={headers}
         rows={rows}
         emptyStateMsg={"No packages found."}
+        className={classes.content}
       />
       <Pagination
         currentPage={currentPage}
