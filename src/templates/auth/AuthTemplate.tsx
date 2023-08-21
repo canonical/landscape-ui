f80185f1c8dda160e595ff924349860ba20d3e23
Yurii Vasyliev <yurii.vasyliev@canonical.com>
2023-08-21 13:12:07 +0300

    Add notifications to login page

--- src/templates/auth/AuthTemplate.tsx
+++ src/templates/auth/AuthTemplate.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from "react";
 import classes from "./AuthTemplate.module.scss";
 import Logo from "../../assets/images/logo-dark-full.svg";
 import { APP_TITLE } from "../../constants";
+import NotifyProvider from "../../context/notify";
 
 interface AuthTemplateProps {
   title: string;
@@ -10,15 +11,17 @@ interface AuthTemplateProps {
 
 const AuthTemplate: FC<AuthTemplateProps> = ({ title, children }) => {
   return (
-    <div className={classes.root}>
-      <div>
-        <img src={Logo} alt={APP_TITLE} width={175} height={56} />
+    <NotifyProvider>
+      <div className={classes.root}>
+        <div>
+          <img src={Logo} alt={APP_TITLE} width={175} height={56} />
+        </div>
+        <div className={classes.inner}>
+          <h1 className={classes.title}>{title}</h1>
+          <div>{children}</div>
+        </div>
       </div>
-      <div className={classes.inner}>
-        <h1 className={classes.title}>{title}</h1>
-        <div>{children}</div>
-      </div>
-    </div>
+    </NotifyProvider>
   );
 };
 
