5b46f81fab074cc644b737cc3b7ab286b642d944
Yurii Vasyliev <yurii.vasyliev@canonical.com>
2023-09-05 19:15:56 +0300

    Add component to search with custom popup help trigger

---
+++ src/components/form/SearchAndFilterWithDescription.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import {
+  SearchAndFilterChip,
+  SearchAndFilterData,
+} from "@canonical/react-components/dist/components/SearchAndFilter/types";
+import { SearchAndFilter } from "@canonical/react-components";
+import classes from "./SearchAndFilterWithDescription.module.scss";
+import classNames from "classnames";
+
+interface SearchAndFilterWithDescriptionProps {
+  filterPanelData: SearchAndFilterData[];
+  returnSearchData: (searchData: SearchAndFilterChip[]) => void;
+  onClick: () => void;
+}
+
+const SearchAndFilterWithDescription: FC<
+  SearchAndFilterWithDescriptionProps
+> = ({ filterPanelData, returnSearchData, onClick }) => {
+  return (
+    <div>
+      <div className={classNames(classes.wrapper)}>
+        <div className={classes.search}>
+          <SearchAndFilter
+            filterPanelData={filterPanelData}
+            returnSearchData={returnSearchData}
+          />
+        </div>
+        <div className={classes.buttonContainer}>
+          <button onClick={onClick} className={classNames(classes.button)}>
+            <i className={classNames("p-icon--help", classes.icon)} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchAndFilterWithDescription;
