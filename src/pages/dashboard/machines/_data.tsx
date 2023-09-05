ee6fa39cf7664f64fc4a772a627f9cf450805da6
Yurii Vasyliev <yurii.vasyliev@canonical.com>
2023-09-05 17:06:50 +0300

    Add helper popup for computer query search

---
+++ src/pages/dashboard/machines/_data.tsx
@@ -0,0 +1,144 @@
+import { Process } from "../../../types/Process";
+
+// Mock data to populate the table
+export const mockProcesses: Process[] = [
+  {
+    pid: 1000,
+    gid: 1,
+    uid: 1,
+    name: "process1",
+    state: 1,
+    start_time: "2021-01-01 00:00:00",
+    vm_size: 10240,
+    cpu_utilization: 0.1,
+    computer_id: 1,
+  },
+  {
+    pid: 1001,
+    gid: 1,
+    uid: 1,
+    name: "process2",
+    state: 10,
+    start_time: "2022-05-07 00:00:00",
+    vm_size: 10240,
+    cpu_utilization: 0.2,
+    computer_id: 1,
+  },
+  {
+    pid: 1002,
+    gid: 2,
+    uid: 2,
+    name: "process3",
+    state: 8,
+    start_time: "2023-10-03 00:00:00",
+    vm_size: 5120,
+    cpu_utilization: 0.18,
+    computer_id: 1,
+  },
+];
+
+export const SEARCH_HELP_TERMS: Record<string, unknown>[] = [
+  {
+    term: "<keyword>",
+    description: (
+      <span>
+        Computers with the title or hostname <code>&lt;keyword&gt;</code>
+      </span>
+    ),
+  },
+  {
+    term: "distribution:<keyword>",
+    description: (
+      <span>
+        Computers with the installed distribution named{" "}
+        <code>&lt;keyword&gt;</code>
+      </span>
+    ),
+  },
+  {
+    term: "needs:reboot",
+    description: "Computers needing a reboot",
+  },
+  {
+    term: "alert:<type>",
+    description: (
+      <>
+        <span>
+          Computers with outstanding alerts of the specified{" "}
+          <code>&lt;type&gt;</code>
+        </span>
+        <br />
+        <span>
+          <code>&lt;type&gt;</code> can be package-upgrades, security-upgrades,
+          package-profiles, package-reporter, esm-disabled, computer-offline,
+          computer-reboot, computer-duplicates, unapproved-activities.
+        </span>
+      </>
+    ),
+  },
+  {
+    term: "ip:<address>",
+    description: (
+      <>
+        <span>
+          Computers with IP addresses like <code>&lt;address&gt;</code>
+        </span>
+        <br />
+        <span>
+          <code>&lt;address&gt;</code> can be a fragment or partial IP address
+          representing a subnet such as 91.185.94 to match any computers in the
+          subnet.
+        </span>
+      </>
+    ),
+  },
+  {
+    term: "mac:<address>",
+    description: (
+      <span>
+        Computers with the MAC address <code>&lt;address&gt;</code>
+      </span>
+    ),
+  },
+  {
+    term: "id:<computer_id>",
+    description: (
+      <span>
+        Computer with the id <code>&lt;computer_id&gt;</code>
+      </span>
+    ),
+  },
+  {
+    term: "access-group:<name>",
+    description: (
+      <span>
+        Computers associated with the <code>&lt;name&gt;</code> access group
+      </span>
+    ),
+  },
+  {
+    term: "search:<name>",
+    description: (
+      <span>
+        Computers meeting the terms of the <code>&lt;name&gt;</code> saved
+        search
+      </span>
+    ),
+  },
+  {
+    term: "upgrade-profile:<name>",
+    description: (
+      <span>
+        Computers associated with the <code>&lt;name&gt;</code> upgrade profile
+      </span>
+    ),
+  },
+  {
+    term: "removal-profile:<name>",
+    description: (
+      <span>
+        Computers associated with the <code>&lt;name&gt;</code> removal profile
+      </span>
+    ),
+  },
+];
