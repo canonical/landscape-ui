13ea8b6fdb70ff3650996ac765a52cf3214a2289
Yurii Vasyliev <yurii.vasyliev@canonical.com>
2023-09-13 15:34:23 +0300

    Change not found page layout

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,7 +1,16 @@
 import { FC } from "react";
+import AuthTemplate from "../templates/auth";
+import { Link } from "react-router-dom";
 
 const PageNotFound: FC = () => {
-  return <div>Page not found.</div>;
+  return (
+    <AuthTemplate title="Page not found">
+      <p>It seems that page you&apos;re looking for doesn&apos;t exist.</p>
+      <Link to={"/"} className="p-button--positive u-no-margin--bottom">
+        Go back to the home page
+      </Link>
+    </AuthTemplate>
+  );
 };
 
 export default PageNotFound;
