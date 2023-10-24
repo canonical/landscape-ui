f44d0c8b429ed1bced58e3ab854b06649c9a0645
Yurii Vasyliev <yurii.vasyliev@canonical.com>
2023-10-24 18:33:25 +0200

    Add icons to user navigation, extend Beta warning block

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const IS_DEV_ENV = import.meta.env.DEV;
 export const API_URL = import.meta.env.VITE_API_URL;
+export const OLD_DASHBOARD_URL = import.meta.env.VITE_OLD_DASHBOARD_URL;
 export const API_VERSION = "2011-08-01";
 export const APP_TITLE = import.meta.env.VITE_APP_TITLE;
 export const DEFAULT_MIRROR_URI = "http://archive.ubuntu.com/ubuntu";
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   readonly VITE_API_URL: string;
+  readonly VITE_OLD_DASHBOARD_URL: string;
 }
 
 interface ImportMeta {
