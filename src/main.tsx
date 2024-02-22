67a95bf054170431da5360438f32f7feabdec46f
Yurii Vasyliev <yurii.vasyliev@canonical.com>
2024-02-22 10:10:31 +0100

    Change default react query config

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import ConfirmProvider from "./context/confirm";
 import AuthProvider from "./context/auth";
+import NotifyProvider from "./context/notify";
+import { StrictMode } from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <ConfirmProvider>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </ConfirmProvider>
+        <NotifyProvider>
+          <ConfirmProvider>
+            <AuthProvider>
+              <App />
+            </AuthProvider>
+          </ConfirmProvider>
+        </NotifyProvider>
       </QueryClientProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 );
