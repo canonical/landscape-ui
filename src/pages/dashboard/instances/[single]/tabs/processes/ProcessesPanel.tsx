552e503d0dc1d25431feb117eb8a072c611bb832
rubinaga <rubinaga02@gmail.com>
2024-03-05 14:12:19 +0100

    (feat): add instance processes page

--- src/pages/dashboard/instances/[single]/tabs/processes/ProcessesPanel.tsx
+++ src/pages/dashboard/instances/[single]/tabs/processes/ProcessesPanel.tsx
@@ -1,189 +1,74 @@
+import TablePagination from "@/components/layout/TablePagination";
+import { ProcessesHeader, ProcessesList } from "@/features/processes";
+import useDebug from "@/hooks/useDebug";
+import { useProcesses } from "@/hooks/useProcesses";
 import { FC, useMemo, useState } from "react";
-import { mockProcesses } from "../../../_data";
-import { CheckboxInput, ModularTable } from "@canonical/react-components";
-import TablePagination from "../../../../../../components/layout/TablePagination";
-import { useParams } from "react-router-dom";
-import useDebug from "../../../../../../hooks/useDebug";
-import { useProcesses } from "../../../../../../hooks/useProcesses";
-import useInstances from "../../../../../../hooks/useInstances";
-import ProcessesPanelHeader from "./ProcessesPanelHeader";
-import { Instance } from "../../../../../../types/Instance";
-import { CellProps, Column } from "react-table";
-import { Process } from "../../../../../../types/Process";
 
-const ProcessesPanel: FC = () => {
+interface ProcessesPanelProps {
+  instanceId: number;
+}
+
+const ProcessesPanel: FC<ProcessesPanelProps> = ({ instanceId }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(50);
   const [selectedPids, setSelectedPids] = useState<number[]>([]);
   const [search, setSearch] = useState("");
 
-  const { hostname } = useParams();
-  const debug = useDebug();
-  const { getInstancesQuery } = useInstances();
   const { getProcessesQuery } = useProcesses();
+  const debug = useDebug();
+  const {
+    data: getProcessesQueryResult,
+    error: getProcessesQueryError,
+    isLoading,
+  } = getProcessesQuery({
+    computer_id: instanceId,
+    limit: pageSize,
+    offset: (currentPage - 1) * pageSize,
+    search: search,
+  });
+
+  if (getProcessesQueryError) {
+    debug(getProcessesQueryError);
+  }
+
+  const totalProcesses = getProcessesQueryResult?.data.count ?? 0;
 
   const handlePaginate = (page: number) => {
     setCurrentPage(page);
     setSelectedPids([]);
   };
 
-  const { data: getInstancesQueryResult, error: getInstancesQueryError } =
-    getInstancesQuery({ query: `hostname:${hostname}` });
-
-  if (getInstancesQueryError) {
-    debug(getInstancesQueryError);
-  }
-
-  const instance = getInstancesQueryResult?.data.results[0] ?? null;
-
-  const { data: getProcessesQueryResult, error: getProcessesQueryError } =
-    getProcessesQuery({ query: `hostname=${hostname}` }, { enabled: false });
-
-  if (getProcessesQueryError) {
-    debug(getProcessesQueryError);
-  }
-
-  const toggleAll = () => {
-    setSelectedPids((prevState) =>
-      0 === prevState.length ? processes.map(({ pid }) => pid) : [],
-    );
-  };
-
-  const getProcesses = (
-    instance: Instance | null,
-    limit: number,
-    offset: number,
-  ) => {
-    if (!instance) {
-      return [];
-    }
-
-    return mockProcesses
-      .filter(({ computer_id }) => computer_id === instance.id)
-      .slice(offset, offset + limit);
-  };
-
   const processes = useMemo(
-    () =>
-      (getProcessesQueryResult
-        ? getProcessesQueryResult.data.results
-        : getProcesses(instance, pageSize, (currentPage - 1) * pageSize)
-      ).filter(({ name }) => name.match(search)),
-    [getProcessesQueryResult, instance, pageSize, currentPage, search],
-  );
-
-  const totalProcesses = mockProcesses.filter(
-    ({ computer_id }) => computer_id === instance?.id,
-  ).length;
-
-  const columns = useMemo<Column<Process>[]>(
-    () => [
-      {
-        accessor: "name",
-        Header: (
-          <>
-            <CheckboxInput
-              inline
-              label={<span className="u-off-screen">Toggle all processes</span>}
-              checked={
-                selectedPids.length > 0 &&
-                selectedPids.length === processes.length
-              }
-              disabled={processes.length === 0}
-              onChange={toggleAll}
-              indeterminate={
-                selectedPids.length > 0 &&
-                selectedPids.length < processes.length
-              }
-            />
-            <span>Command</span>
-          </>
-        ),
-        Cell: ({ row }: CellProps<Process>) => {
-          return (
-            <>
-              <CheckboxInput
-                inline
-                label={
-                  <span className="u-off-screen">{`Select process ${row.original.name}`}</span>
-                }
-                checked={selectedPids.includes(row.original.pid)}
-                onChange={() => {
-                  setSelectedPids((prevState) => {
-                    return prevState.includes(row.original.pid)
-                      ? prevState.filter(
-                          (prevStatePid) => prevStatePid !== row.original.pid,
-                        )
-                      : [...prevState, row.original.pid];
-                  });
-                }}
-              />
-              <span>{row.original.name}</span>
-            </>
-          );
-        },
-      },
-      {
-        accessor: "state",
-        Header: "State",
-      },
-      {
-        accessor: "vm_size",
-        Header: "VM size",
-      },
-      {
-        accessor: "cpu_utilization",
-        Header: "CPU (%)",
-        Cell: ({ row }: CellProps<Process>) => {
-          return (100 * row.original.cpu_utilization).toFixed(1);
-        },
-      },
-      {
-        accessor: "pid",
-        Header: "PID",
-      },
-      {
-        accessor: "start_time",
-        Header: "Started at",
-      },
-      {
-        accessor: "uid",
-        Header: "User",
-      },
-      {
-        accessor: "gid",
-        Header: "Group",
-      },
-    ],
-    [processes, selectedPids.length],
+    () => getProcessesQueryResult?.data.results ?? [],
+    [getProcessesQueryResult],
   );
 
   return (
-    instance && (
-      <>
-        <ProcessesPanelHeader
-          instanceId={instance.id}
-          onPageChange={handlePaginate}
-          onSearch={(searchText) => {
-            setSearch(searchText);
-          }}
-          selectedPids={selectedPids}
-        />
-        <ModularTable
-          columns={columns}
-          data={processes}
-          emptyMsg="No processes"
-        />
-        <TablePagination
-          currentPage={currentPage}
-          totalItems={totalProcesses}
-          paginate={handlePaginate}
-          pageSize={pageSize}
-          setPageSize={setPageSize}
-          currentItemCount={processes.length}
-        />
-      </>
-    )
+    <>
+      <ProcessesHeader
+        instanceId={instanceId}
+        onPageChange={handlePaginate}
+        onSearch={(searchText) => {
+          setSearch(searchText);
+        }}
+        selectedPids={selectedPids}
+        setSelectedPids={(pids) => setSelectedPids(pids)}
+      />
+      <ProcessesList
+        isLoading={isLoading}
+        processes={processes}
+        setSelectedPids={(pids) => setSelectedPids(pids)}
+        selectedPids={selectedPids}
+      />
+      <TablePagination
+        currentPage={currentPage}
+        totalItems={totalProcesses}
+        paginate={handlePaginate}
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+        currentItemCount={processes.length}
+      />
+    </>
   );
 };
 
--- src/types/Process.d.ts
+++ src/types/Process.d.ts
@@ -1,7 +1,8 @@
 export interface Process extends Record<string, unknown> {
   computer_id: number;
-  cpu_utilization: number;
+  cpu_utilisation: number;
   gid: number;
+  id: number;
   name: string;
   pid: number;
   start_time: string;
