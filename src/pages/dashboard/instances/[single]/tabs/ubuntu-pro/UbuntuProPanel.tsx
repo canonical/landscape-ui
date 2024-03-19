49e2bf6a6943a1154a9b1bb9630bf3601bace401
rubinaga <rubinaga02@gmail.com>
2024-03-19 18:16:58 +0100

    (feat): implement Ubuntu Pro panel

---
+++ src/pages/dashboard/instances/[single]/tabs/ubuntu-pro/UbuntuProPanel.tsx
@@ -0,0 +1,54 @@
+import { FC } from "react";
+import { Instance } from "@/types/Instance";
+import { UbuntuProHeader, UbuntuProList } from "@/features/ubuntupro";
+import EmptyState from "@/components/layout/EmptyState";
+import { Button, Link } from "@canonical/react-components";
+import classes from "./UbuntuProPanel.module.scss";
+
+interface UbuntuProPanelProps {
+  instance: Instance;
+}
+
+const UbunuProPanel: FC<UbuntuProPanelProps> = ({ instance }) => {
+  return instance.ubuntu_pro_info ? (
+    <>
+      <UbuntuProHeader data={instance.ubuntu_pro_info} />
+      <UbuntuProList items={instance.ubuntu_pro_info.services} />
+    </>
+  ) : (
+    <EmptyState
+      title="No Ubuntu Pro entitlement"
+      body={
+        <>
+          <p>
+            This computer is not currently attached to an Ubuntu Pro
+            entitlement, which provides additional security updates and other
+            benefits from Canonical
+          </p>
+          <Link
+            href="https://ubuntu.com/pro"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+          >
+            Learn more
+          </Link>
+        </>
+      }
+      cta={[
+        <Link
+          key="attach-ubuntu-pro"
+          href="http://ubuntu.com/pro/dashboard"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          className={classes.attachUbuntuProButton}
+        >
+          <Button appearance="positive" type="button" aria-label="Go back">
+            Attach Ubuntu Pro
+          </Button>
+        </Link>,
+      ]}
+    />
+  );
+};
+
+export default UbunuProPanel;
