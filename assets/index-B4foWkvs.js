a8fd5147e0c0ae47a8c5d9ba57d8e3930bcb8cb3
Mitch Burton <mitch.burton@canonical.com>
2024-07-09 08:28:06 +0000

    automated build

---
+++ assets/index-B4foWkvs.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BtF_YtUH.js";import{P as s,a as r,b as a}from"./PageMain-BrvrZMoX.js";const i=()=>e.jsxs(s,{children:[e.jsx(r,{title:"WSL Profiles"}),e.jsx(a,{children:e.jsx("div",{children:"WSL Profiles"})})]}),n=i;export{n as default};
---
+++ assets/useFetchOld-BGOduMhT.js
@@ -0,0 +1,1 @@
+import{r as t,F as e}from"./index-BtF_YtUH.js";function o(){return t.useContext(e)}export{o as u};
