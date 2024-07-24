9884d9e86c02f7d59448137a440f888bf2fb528e
Mitch Burton <mitch.burton@canonical.com>
2024-07-24 13:47:11 +0000

    automated build

---
+++ assets/useConfirm-DtGSn1sv.js
@@ -0,0 +1,1 @@
+import{r,C as t}from"./index-aXCRV7My.js";function e(){return r.useContext(t)}export{e as u};
---
+++ assets/useFetchOld-Dx05gkab.js
@@ -0,0 +1,1 @@
+import{r as t,F as e}from"./index-aXCRV7My.js";function o(){return t.useContext(e)}export{o as u};
