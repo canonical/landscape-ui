4228dcda977fb2bd455fa178161802ba4be995f1
Yurii Vasyliev <yurii.vasyliev@canonical.com>
2024-12-02 14:53:29 +0100

    style: adjust Redirecting component styles

--- src/components/layout/Redirecting/Redirecting.tsx
+++ src/components/layout/Redirecting/Redirecting.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
+import classes from "./Redirecting.module.scss";
 
 const Redirecting: FC = () => {
   return (
-    <div className="u-align-text--center">
-      <span role="status" style={{ marginRight: "1rem" }}>
+    <div className={classes.root}>
+      <span role="status" className={classes.icon}>
         <span className="u-off-screen">Redirecting...</span>
         <i className="p-icon--spinner u-animation--spin" aria-hidden />
       </span>
