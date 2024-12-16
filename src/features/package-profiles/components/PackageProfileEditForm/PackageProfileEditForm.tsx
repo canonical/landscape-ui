053e5b444cca5e6fb583e35bdc1045656e832067
Yurii Vasyliev <113896226+yurii-vasyliev@users.noreply.github.com>
2024-12-16 18:01:35 +0200

    fix: remove access group dropdown from edit package profile form (#155)
    - remove access group dropdown
    - improve the way we test errors

--- src/features/package-profiles/components/PackageProfileEditForm/PackageProfileEditForm.tsx
+++ src/features/package-profiles/components/PackageProfileEditForm/PackageProfileEditForm.tsx
@@ -1,12 +1,11 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { EditFormProps, PackageProfile } from "../../types";
 import useDebug from "@/hooks/useDebug";
 import useSidePanel from "@/hooks/useSidePanel";
 import { usePackageProfiles } from "../../hooks";
 import { useFormik } from "formik";
-import useRoles from "@/hooks/useRoles";
-import { Form, Input, Select } from "@canonical/react-components";
-import { INITIAL_VALUES, VALIDATION_SCHEMA } from "./constants";
+import { Form, Input } from "@canonical/react-components";
+import { VALIDATION_SCHEMA } from "./constants";
 import SidePanelFormButtons from "@/components/form/SidePanelFormButtons";
 import AssociationBlock from "@/components/form/AssociationBlock";
 import useNotify from "@/hooks/useNotify";
@@ -22,15 +21,6 @@ const PackageProfileEditForm: FC<PackageProfileEditFormProps> = ({
   const { notify } = useNotify();
   const { closeSidePanel } = useSidePanel();
   const { editPackageProfileQuery } = usePackageProfiles();
-  const { getAccessGroupQuery } = useRoles();
-
-  const { data: getAccessGroupQueryResult } = getAccessGroupQuery();
-
-  const accessGroupOptions =
-    getAccessGroupQueryResult?.data.map(({ name, title }) => ({
-      label: title,
-      value: name,
-    })) ?? [];
 
   const { mutateAsync: editPackageProfile } = editPackageProfileQuery;
 
@@ -53,25 +43,17 @@ const PackageProfileEditForm: FC<PackageProfileEditFormProps> = ({
     }
   };
 
-  const formik = useFormik({
-    initialValues: INITIAL_VALUES,
-    onSubmit: handleSubmit,
-    validationSchema: VALIDATION_SCHEMA,
-  });
-
-  useEffect(() => {
-    if (!profile) {
-      return;
-    }
-
-    formik.setValues({
-      access_group: profile.access_group,
+  const formik = useFormik<EditFormProps>({
+    enableReinitialize: true,
+    initialValues: {
       all_computers: profile.all_computers,
       description: profile.description,
       tags: profile.tags,
       title: profile.title,
-    });
-  }, [profile]);
+    },
+    onSubmit: handleSubmit,
+    validationSchema: VALIDATION_SCHEMA,
+  });
 
   return (
     <Form onSubmit={formik.handleSubmit} noValidate>
@@ -90,21 +72,6 @@ const PackageProfileEditForm: FC<PackageProfileEditFormProps> = ({
         type="text"
         label="Description"
         {...formik.getFieldProps("description")}
-        error={
-          formik.touched.description && formik.errors.description
-            ? formik.errors.description
-            : undefined
-        }
-      />
-
-      <Select
-        label="Access group"
-        options={accessGroupOptions}
-        {...formik.getFieldProps("access_group")}
-        error={
-          (formik.touched.access_group && formik.errors.access_group) ||
-          undefined
-        }
       />
 
       <AssociationBlock formik={formik} />
