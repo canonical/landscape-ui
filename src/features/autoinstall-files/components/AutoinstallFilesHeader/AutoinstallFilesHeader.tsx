ffe6e1186104f2122aea21c1b7c100f23b378791
Ethan Shaw <ethan.shaw@canonical.com>
2024-12-18 15:41:23 -0700

    feat: add side panel for adding new autoinstall file

--- src/features/autoinstall-files/components/AutoinstallFilesHeader/AutoinstallFilesHeader.tsx
+++ src/features/autoinstall-files/components/AutoinstallFilesHeader/AutoinstallFilesHeader.tsx
@@ -3,11 +3,17 @@ import { usePageParams } from "@/hooks/usePageParams";
 import { FC } from "react";
 import classes from "./AutoinstallFilesHeader.module.scss";
 import { TableFilter, TableFilterChips } from "@/components/filter";
-import { Button, Icon } from "@canonical/react-components";
-import { AUTOINSTALL_FILE_EMPLOYEE_GROUP_OPTIONS } from "./constants";
+import { Button, Form, Icon, Input } from "@canonical/react-components";
+import {
+  ADD_BUTTON_TEXT,
+  AUTOINSTALL_FILE_EMPLOYEE_GROUP_OPTIONS,
+} from "./constants";
+import useSidePanel from "@/hooks/useSidePanel";
+import SidePanelFormButtons from "@/components/form/SidePanelFormButtons";
 
 const AutoinstallFilesHeader: FC = () => {
   const { setPageParams, status } = usePageParams();
+  const { setSidePanelContent } = useSidePanel();
 
   const handleSearch = (searchText: string) => {
     setPageParams({ search: searchText });
@@ -30,9 +36,43 @@ const AutoinstallFilesHeader: FC = () => {
           />
         </div>
 
-        <Button hasIcon={true}>
+        <Button
+          hasIcon={true}
+          onClick={() =>
+            setSidePanelContent(
+              "Add new autoinstall file",
+              <Form>
+                <div className={classes.container}>
+                  <span>
+                    Add autoinstall file. It can be applied during the initial
+                    setup of associated instances.
+                  </span>
+                  <div className={classes.radioGroup}>
+                    <Input
+                      type="radio"
+                      label="From a file"
+                      name="add-method"
+                      value="from-a-file"
+                      defaultChecked={true}
+                    />
+                    <Input
+                      type="radio"
+                      label="Plain text"
+                      name="add-method"
+                      value="plain-text"
+                    />
+                  </div>
+                </div>
+                <SidePanelFormButtons
+                  submitButtonDisabled={false}
+                  submitButtonText="Add"
+                />
+              </Form>,
+            )
+          }
+        >
           <Icon name="plus" />
-          <span>Add new</span>
+          <span>{ADD_BUTTON_TEXT}</span>
         </Button>
       </div>
 
--- src/features/autoinstall-files/components/AutoinstallFilesHeader/constants.ts
+++ src/features/autoinstall-files/components/AutoinstallFilesHeader/constants.ts
@@ -4,3 +4,5 @@ export const AUTOINSTALL_FILE_EMPLOYEE_GROUP_OPTIONS: SelectOption[] = [
   { label: "All", value: "" },
   { label: "Test", value: "test" },
 ];
+
+export const ADD_BUTTON_TEXT = "Add new";
