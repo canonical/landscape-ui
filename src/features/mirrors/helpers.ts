273743b594c0e83fad5701864f0805998f3f2f68
Ethan Shaw <ethan.shaw@canonical.com>
2025-02-19 12:26:03 -0700

    refactor: supress no magic numbers for pluralize

--- src/features/mirrors/helpers.ts
+++ src/features/mirrors/helpers.ts
@@ -1,9 +1,8 @@
-const ONE = 1;
-
 export const pluralize = <S extends string, P extends string>(
   count: number,
   singularForm: S,
   pluralForm: P,
 ): S | P => {
-  return count === ONE ? singularForm : pluralForm;
+  // eslint-disable-next-line no-magic-numbers
+  return count === 1 ? singularForm : pluralForm;
 };
