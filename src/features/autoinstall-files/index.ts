145bb6b0762b02e5cdf35ed8229961b52b8c9f4c
Ethan Shaw <ethan.shaw@canonical.com>
2025-03-10 12:21:37 -0700

    move autoinstall file panel

--- src/features/autoinstall-files/index.ts
+++ src/features/autoinstall-files/index.ts
@@ -1,2 +1,5 @@
-export { default as AutoinstallFilesPanel } from "./components/AutoinstallFilesPanel";
+export { useAddAutoinstallFile, useGetAutoinstallFiles } from "./api";
+export { default as AutoinstallFileForm } from "./components/AutoinstallFileForm";
+export { default as AutoinstallFilesHeader } from "./components/AutoinstallFilesHeader";
+export { default as AutoinstallFilesList } from "./components/AutoinstallFilesList";
 export type { AutoinstallFile, WithGroups } from "./types";
