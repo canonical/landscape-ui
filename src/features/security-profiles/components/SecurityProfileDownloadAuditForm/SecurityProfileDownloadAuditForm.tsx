91321faf865e84937270ba4c62db75abb50f4431
Ethan Shaw <ethan.shaw@canonical.com>
2025-04-09 13:02:17 -0700

    add date pickers

--- src/features/security-profiles/components/SecurityProfileDownloadAuditForm/SecurityProfileDownloadAuditForm.tsx
+++ src/features/security-profiles/components/SecurityProfileDownloadAuditForm/SecurityProfileDownloadAuditForm.tsx
@@ -1,15 +1,25 @@
 import RadioGroup from "@/components/form/RadioGroup";
+import { Input } from "@canonical/react-components";
 import { useFormik } from "formik";
 import type { FC } from "react";
+import classes from "./SecurityProfileDownloadAuditForm.module.scss";
+
+interface FormikProps {
+  audit_timeframe: "specific-date" | "date-range";
+  date_end: string;
+  date_start: string;
+  level_of_detail: "summary-only" | "detailed-view";
+  specific_date: string;
+}
 
 const SecurityProfileDownloadAuditForm: FC = () => {
-  const formik = useFormik<{
-    level_of_detail: "summary-only" | "detailed-view";
-    audit_timeframe: "specific-date" | "date-range";
-  }>({
+  const formik = useFormik<FormikProps>({
     initialValues: {
-      level_of_detail: "summary-only",
       audit_timeframe: "specific-date",
+      date_end: "",
+      date_start: "",
+      level_of_detail: "summary-only",
+      specific_date: "",
     },
     onSubmit: () => undefined,
   });
@@ -23,8 +33,27 @@ const SecurityProfileDownloadAuditForm: FC = () => {
         field="audit_timeframe"
         formik={formik}
         inputs={[
-          { key: "specific-date", label: "Specific date" },
-          { key: "date-range", label: "Date range" },
+          {
+            key: "specific-date",
+            label: "Specific date",
+            expansion: <Input type="datetime-local" />,
+          },
+          {
+            key: "date-range",
+            label: "Date range",
+            expansion: (
+              <div className={classes.dateRange}>
+                <div className={classes.date}>
+                  <Input type="date" />
+                </div>
+                <span className={classes.separator}>-</span>
+
+                <div className={classes.date}>
+                  <Input type="date" />
+                </div>
+              </div>
+            ),
+          },
         ]}
       />
 
