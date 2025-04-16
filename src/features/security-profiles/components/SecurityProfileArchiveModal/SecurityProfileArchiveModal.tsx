d8a845e50a62bed2a995cde4e9e6d69401c08847
Ethan Shaw <ethan.shaw@canonical.com>
2025-04-16 13:19:46 -0700

    text confirmation modal

---
+++ src/features/security-profiles/components/SecurityProfileArchiveModal/SecurityProfileArchiveModal.tsx
@@ -0,0 +1,60 @@
+import TextConfirmationModal from "@/components/form/TextConfirmationModal";
+import useNotify from "@/hooks/useNotify";
+import type { ComponentProps, FC } from "react";
+import { useArchiveSecurityProfile } from "../../api";
+import type { SecurityProfile } from "../../types";
+
+interface SecurityProfileArchiveModalProps
+  extends Pick<ComponentProps<typeof TextConfirmationModal>, "close"> {
+  readonly profile: SecurityProfile | null;
+}
+
+const SecurityProfileArchiveModal: FC<SecurityProfileArchiveModalProps> = ({
+  close,
+  profile,
+}) => {
+  const { notify } = useNotify();
+
+  const { archiveSecurityProfile, isArchivingSecurityProfile } =
+    useArchiveSecurityProfile();
+
+  if (!profile) {
+    return;
+  }
+
+  const onConfirm = async () => {
+    await archiveSecurityProfile({
+      id: profile.id,
+    });
+  };
+
+  const onSuccess = () => {
+    notify.success({
+      title: `You have archived "${profile.title}" profile`,
+      message:
+        "It will no longer run, but past audit data and profile details will remain accessible for selected duration of the retention period.",
+    });
+  };
+
+  return (
+    <TextConfirmationModal
+      title={`Archive "${profile.title}" profile`}
+      close={close}
+      confirmButtonLabel="Archive"
+      confirmationText={`archive ${profile.title}`}
+      confirming={isArchivingSecurityProfile}
+      onConfirm={onConfirm}
+      onSuccess={onSuccess}
+    >
+      <p>
+        You are about to archive the {profile.title} profile. Archiving this
+        security profile will prevent it from running. However, it will{" "}
+        <strong>NOT</strong> delete past audit data or remove the profile
+        details. You will not be able to reactivate the profile after it has
+        been archived.
+      </p>
+    </TextConfirmationModal>
+  );
+};
+
+export default SecurityProfileArchiveModal;
---
+++ src/features/security-profiles/constants.tsx
@@ -0,0 +1,31 @@
+import { Tooltip } from "@canonical/react-components";
+import classes from "./constants.module.scss";
+
+export const SECURITY_PROFILE_STATUSES = {
+  active: { label: "Active", icon: "status-succeeded-small" },
+  archived: { label: "Archived", icon: "status-queued-small" },
+  "over-limit": {
+    label: (
+      <div className={classes.statusWithIcon}>
+        <span>Over limit</span>
+        <span className={classes.iconWrapper}>
+          <Tooltip
+            position="top-center"
+            message="Only the first 5,000 instances are covered. Instances beyond the limit are not covered."
+          >
+            <i className="p-icon--help" role="img" aria-label="Help icon" />
+          </Tooltip>
+        </span>
+      </div>
+    ),
+    icon: "status-failed-small",
+  },
+};
+
+export const SECURITY_PROFILE_BENCHMARK_LABELS = {
+  cis_level1_workstation: "CIS Level 1 Workstation",
+  cis_level1_server: "CIS Level 1 Server",
+  cis_level2_workstation: "CIS Level 2 Workstation",
+  cis_level2_server: "CIS Level 2 Server",
+  disa_stig: "DISA-STIG",
+};
