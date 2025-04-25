8a44a7e9336a33b45b066ae7a2a96421c5e3a058
kyivcanonical <kyiv.edwards@canonical.com>
2025-04-25 10:45:05 -0400

    Improving filter chip text (#232)
    - Making filter chip text more descriptive.
    - Change "passrate" to "pass rate"

--- src/components/filter/TableFilterChips/TableFilterChips.tsx
+++ src/components/filter/TableFilterChips/TableFilterChips.tsx
@@ -306,7 +306,7 @@ const TableFilterChips: FC<TableFilterChipsProps> = ({
         )}
         {renderResults.isPassRateFromChipRender && (
           <Chip
-            value={`From: ${passRateFrom}`}
+            value={`From pass rate: ${passRateFrom}%`}
             onDismiss={() => {
               setPageParams({ passRateFrom: 0 });
             }}
@@ -315,7 +315,7 @@ const TableFilterChips: FC<TableFilterChipsProps> = ({
         )}
         {renderResults.isPassRateToChipRender && (
           <Chip
-            value={`To: ${passRateTo}`}
+            value={`To pass rate: ${passRateTo}%`}
             onDismiss={() => {
               setPageParams({ passRateTo: 100 });
             }}
