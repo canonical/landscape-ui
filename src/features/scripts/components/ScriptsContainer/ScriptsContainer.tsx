54ce0c16117db5af07a1caf6fc1dda76a22ef5be
Ethan Shaw <ethan.shaw@canonical.com>
2025-06-27 12:21:32 -0700

    add script warning

--- src/features/scripts/components/ScriptsContainer/ScriptsContainer.tsx
+++ src/features/scripts/components/ScriptsContainer/ScriptsContainer.tsx
@@ -1,19 +1,28 @@
 import LoadingState from "@/components/layout/LoadingState";
 import { TablePagination } from "@/components/layout/TablePagination";
+import { redirectToExternalUrl } from "@/features/auth";
 import {
   ScriptList,
   ScriptsEmptyState,
   useGetScripts,
 } from "@/features/scripts";
+import useFetch from "@/hooks/useFetch";
 import usePageParams from "@/hooks/usePageParams";
+import { Button, Notification } from "@canonical/react-components";
 import type { FC } from "react";
+import { useBoolean } from "usehooks-ts";
 import ScriptsHeader from "../ScriptsHeader";
+import { SCRIPT_NOTIFICATION_LOCAL_STORAGE_ITEM } from "./constants";
 import { isScriptsEmptyState, isScriptsLoadingState } from "./helpers";
 
 const ScriptsContainer: FC = () => {
+  const authFetch = useFetch();
   const { currentPage, pageSize, search, status } = usePageParams();
 
   const { scripts, scriptsCount, isScriptsLoading } = useGetScripts();
+
+  const { value: isNotificationVisible, setFalse: hideNotification } =
+    useBoolean(!localStorage.getItem(SCRIPT_NOTIFICATION_LOCAL_STORAGE_ITEM));
 
   if (
     isScriptsEmptyState(
@@ -28,8 +37,32 @@ const ScriptsContainer: FC = () => {
     return <ScriptsEmptyState />;
   }
 
+  const dismiss = () => {
+    hideNotification();
+    localStorage.setItem(SCRIPT_NOTIFICATION_LOCAL_STORAGE_ITEM, "true");
+  };
+
   return (
     <>
+      {isNotificationVisible && (
+        <Notification onDismiss={dismiss}>
+          <strong>This page only displays v2 scripts.</strong> To access older
+          (v1) scripts, go to{" "}
+          <Button
+            appearance="link"
+            onClick={async () => {
+              redirectToExternalUrl(
+                (await authFetch.get<{ url: string }>("classic_dashboard_url"))
+                  .data.url,
+              );
+            }}
+          >
+            the legacy web portal
+          </Button>
+          .
+        </Notification>
+      )}
+
       <ScriptsHeader />
       {isScriptsLoadingState(currentPage, pageSize, isScriptsLoading) ? (
         <LoadingState />
