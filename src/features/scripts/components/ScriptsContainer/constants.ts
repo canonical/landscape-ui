6a96a141320b2dd95a32a7fe0291ddf11a7336b3
Ethan Shaw <ethan.shaw@canonical.com>
2025-07-01 11:12:54 -0700

    fix: show scripts warning (#293)

--- src/features/scripts/components/ScriptsContainer/constants.ts
+++
@@ -1,2 +0,0 @@
-export const SCRIPT_NOTIFICATION_LOCAL_STORAGE_ITEM =
-  "_landscape_isScriptNotificationDismissed";
--- src/pages/dashboard/scripts/ScriptsPage/ScriptsPage.tsx
+++ src/pages/dashboard/scripts/ScriptsPage/ScriptsPage.tsx
@@ -1,17 +1,43 @@
 import PageContent from "@/components/layout/PageContent";
 import PageHeader from "@/components/layout/PageHeader";
 import PageMain from "@/components/layout/PageMain";
+import { redirectToExternalUrl } from "@/features/auth";
 import { ScriptsContainer, ScriptsTabs } from "@/features/scripts";
 import useAuth from "@/hooks/useAuth";
+import useFetch from "@/hooks/useFetch";
+import { Button, Notification } from "@canonical/react-components";
 import type { FC } from "react";
 
 const ScriptsPage: FC = () => {
   const { isFeatureEnabled } = useAuth();
+  const authFetch = useFetch();
 
   return (
     <PageMain>
       <PageHeader title="Scripts" />
       <PageContent>
+        <Notification severity="caution">
+          <strong>This page only displays v2 scripts.</strong> Older (v1)
+          scripts can be found in{" "}
+          <Button
+            appearance="link"
+            onClick={async () => {
+              redirectToExternalUrl(
+                `${
+                  (
+                    await authFetch.get<{ url: string }>(
+                      "classic_dashboard_url",
+                    )
+                  ).data.url
+                }/scripts`,
+              );
+            }}
+          >
+            the legacy web portal
+          </Button>
+          .
+        </Notification>
+
         {isFeatureEnabled("script-profiles") ? (
           <ScriptsTabs />
         ) : (
