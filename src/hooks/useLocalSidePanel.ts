30bb84601620b79ae6f1bdd9079c61fb1286f599
Ethan Shaw <ethan.shaw@canonical.com>
2025-07-02 10:21:32 -0700

    refactoring

--- src/hooks/useLocalSidePanel.ts
+++
@@ -1,6 +0,0 @@
-import { LocalSidePanelContext } from "@/context/localSidePanel/localSidePanel";
-import { useContext } from "react";
-
-export default function useLocalSidePanel() {
-  return useContext(LocalSidePanelContext);
-}
