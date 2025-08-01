391116031b0f5658e900e5615e85f3c602c90e1f
Bill Kronholm <bill.kronholm@canonical.com>
2025-08-01 11:22:40 -0700

    release: 1.16.0

---
+++ assets/SidePanelFormButtons.module-BuIu-Bem.js
@@ -0,0 +1,1 @@
+const t="_buttons_r5h68_1",o="_actionButtons_r5h68_12",n={buttons:t,actionButtons:o};export{n as b};
--- assets/index-Bp31lJzT.js
+++
@@ -1,1 +0,0 @@
-import{a as r,r as t}from"./index-gc_gETRV.js";const s=()=>{const e=r();return t.useEffect(()=>{e("/repositories/mirrors",{replace:!0})},[]),null};export{s as default};
