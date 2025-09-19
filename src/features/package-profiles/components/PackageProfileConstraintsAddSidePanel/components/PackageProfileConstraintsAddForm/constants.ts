dc22865d54e5bad79bc31bd6b145b20822c9f4e7
Rubin Aga <66167934+rubinaga@users.noreply.github.com>
2025-09-19 16:26:18 +0200

    refactor: remove code duplication for package profile constraints validation schema (#373)
    ## Items
    - refactor code duplication in package profile constraints validation schemas

--- src/features/package-profiles/components/PackageProfileConstraintsAddSidePanel/components/PackageProfileConstraintsAddForm/constants.ts
+++ src/features/package-profiles/components/PackageProfileConstraintsAddSidePanel/components/PackageProfileConstraintsAddForm/constants.ts
@@ -1,21 +1,6 @@
+import { constraintsSchema } from "../../../../constants";
 import * as Yup from "yup";
 
 export const VALIDATION_SCHEMA = Yup.object().shape({
-  constraints: Yup.array()
-    .of(
-      Yup.object().shape({
-        constraint: Yup.string().required("Required."),
-        notAnyVersion: Yup.boolean(),
-        package: Yup.string().required("Required."),
-        rule: Yup.string().when("notAnyVersion", {
-          is: true,
-          then: (schema) => schema.required("Required."),
-        }),
-        version: Yup.string().when("notAnyVersion", {
-          is: true,
-          then: (schema) => schema.required("Required."),
-        }),
-      }),
-    )
-    .min(1, "Package profiles must have at least one package constraint."),
+  constraints: constraintsSchema,
 });
