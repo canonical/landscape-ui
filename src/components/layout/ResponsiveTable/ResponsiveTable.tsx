4bb021756157389a80a0f58a78c3aa2f09df6475
Ethan Shaw <ethan.shaw@canonical.com>
2025-10-21 13:21:26 -0700

    sticky first column

--- src/components/layout/ResponsiveTable/ResponsiveTable.tsx
+++ src/components/layout/ResponsiveTable/ResponsiveTable.tsx
@@ -1,8 +1,11 @@
-import type { JSX } from "react";
+import { BREAKPOINT_PX } from "@/constants";
 import {
   ModularTable,
   type ModularTableProps,
 } from "@canonical/react-components";
+import classNames from "classnames";
+import type { JSX } from "react";
+import { useMediaQuery } from "usehooks-ts";
 import classes from "./ResponsiveTable.module.scss";
 
 export interface ResponsiveTableProps<
@@ -17,11 +20,29 @@ export default function ResponsiveTable<
   ref,
   style,
   minWidth = 1024,
+  columns,
   ...tableProps
 }: ResponsiveTableProps<Row>): JSX.Element {
+  const isSmall = useMediaQuery(`(max-width: ${BREAKPOINT_PX.sm}px)`);
+
+  columns = isSmall
+    ? columns
+    : [
+        {
+          ...columns[0],
+          className: classNames(columns[0].className, classes.firstColumn),
+        },
+        ...columns.slice(1),
+      ];
+
   return (
     <div className={classes.responsiveTable} style={style}>
-      <ModularTable ref={ref} style={{ minWidth }} {...tableProps} />
+      <ModularTable
+        ref={ref}
+        style={{ minWidth }}
+        columns={columns}
+        {...tableProps}
+      />
     </div>
   );
 }
