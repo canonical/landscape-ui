cc78023d70cebea676f854b5c0d9d66a5d17b486
Ethan Shaw <ethan.shaw@canonical.com>
2025-10-23 10:34:51 -0700

    add shadows

--- src/components/layout/ResponsiveTable/ResponsiveTable.tsx
+++ src/components/layout/ResponsiveTable/ResponsiveTable.tsx
@@ -4,7 +4,8 @@ import {
   type ModularTableProps,
 } from "@canonical/react-components";
 import classNames from "classnames";
-import type { JSX } from "react";
+import type { UIEventHandler } from "react";
+import { useState, type JSX } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import classes from "./ResponsiveTable.module.scss";
 
@@ -17,43 +18,56 @@ export interface ResponsiveTableProps<
 export default function ResponsiveTable<
   Row extends Record<string, unknown> = Record<string, unknown>,
 >({
-  ref,
   style,
   minWidth = 1024,
-  columns,
-  getHeaderProps,
   ...tableProps
 }: ResponsiveTableProps<Row>): JSX.Element {
   const isSmall = useMediaQuery(`(max-width: ${BREAKPOINT_PX.sm}px)`);
 
-  columns = isSmall
-    ? columns
+  const [isScrolledDown, setIsScrolledDown] = useState(false);
+  const [isScrolledRight, setIsScrolledRight] = useState(false);
+
+  const className = classNames(classes.responsiveTable, {
+    [classes.scrolledDown]: isScrolledDown,
+  });
+
+  const handleScroll: UIEventHandler = (event) => {
+    setIsScrolledDown(event.currentTarget.scrollTop > 0);
+    setIsScrolledRight(event.currentTarget.scrollLeft > 0);
+  };
+
+  const columns = isSmall
+    ? tableProps.columns
     : [
         {
-          ...columns[0],
-          className: classNames(columns[0].className, classes.firstColumn),
+          ...tableProps.columns[0],
+          className: classNames(
+            tableProps.columns[0].className,
+            classes.firstColumn,
+            {
+              [classes.scrolledRight]: isScrolledRight,
+            },
+          ),
         },
-        ...columns.slice(1),
+        ...tableProps.columns.slice(1),
       ];
 
-  const getHeaderPropsAdjusted: ModularTableProps<Row>["getHeaderProps"] = (
-    header,
-  ) => {
-    const headerProps = getHeaderProps ? getHeaderProps(header) : {};
+  const getHeaderProps: ModularTableProps<Row>["getHeaderProps"] = (header) => {
+    const headerProps = tableProps.getHeaderProps
+      ? tableProps.getHeaderProps(header)
+      : {};
 
     headerProps.className = classNames(headerProps.className, classes.firstRow);
-
     return headerProps;
   };
 
   return (
-    <div className={classes.responsiveTable} style={style}>
+    <div className={className} onScroll={handleScroll} style={style}>
       <ModularTable
-        ref={ref}
-        style={{ minWidth }}
-        columns={columns}
-        getHeaderProps={getHeaderPropsAdjusted}
         {...tableProps}
+        columns={columns}
+        getHeaderProps={getHeaderProps}
+        style={{ minWidth }}
       />
     </div>
   );
