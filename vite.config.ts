4b3c962117b00bf17cb6df186c4a165011837112
rubinaga <rubinaga02@gmail.com>
2025-11-14 12:22:56 +0100

    chore: suppress scss deprecation warnings

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         quietDeps: true,
+        silenceDeprecations: ["import", "global-builtin"],
       },
     },
   },
