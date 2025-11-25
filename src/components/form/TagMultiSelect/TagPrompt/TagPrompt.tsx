b9ca6c0f64da9ed81635ba8d1c302e7c20a1f9d8
Yurii Vasyliev <113896226+yurii-vasyliev@users.noreply.github.com>
2025-11-25 19:21:48 +0100

    fix: revert TagMultiSelect component (#424)
    Revert TagMultiSelect component. We're showing it again inside the Edit
    instance side panel in order to be able to add new tags.

---
+++ src/components/form/TagMultiSelect/TagPrompt/TagPrompt.tsx
@@ -0,0 +1,35 @@
+import type { FC } from "react";
+import { Button } from "@canonical/react-components";
+import classes from "./TagPrompt.module.scss";
+
+interface TagPromptProps {
+  readonly onTagAdd: () => void;
+  readonly search: string;
+}
+
+const TagPrompt: FC<TagPromptProps> = ({ onTagAdd, search }) => {
+  return (
+    <>
+      {search && (
+        <div className={classes.container}>
+          <span className={classes.prompt}>
+            <span>Search for </span>
+            <span className="p-search-and-filter__search-query">{search}</span>
+            <span>...</span>
+          </span>
+
+          <Button
+            type="button"
+            appearance="link"
+            onClick={onTagAdd}
+            className={classes.saveButton}
+          >
+            Add tag
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default TagPrompt;
