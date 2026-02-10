ee53c9c823aa617dbc07d3302dd6f4ffb6ee7dae
Gabe Gaghi <gabe.gaghi@canonical.com>
2026-02-10 15:28:45 -0300

    move profiles components into profiles feature

--- src/features/removal-profiles/index.ts
+++ src/features/removal-profiles/index.ts
@@ -1,8 +1,5 @@
 export { default as RemovalProfileAddSidePanel } from "./components/RemovalProfileAddSidePanel";
 export { default as RemovalProfileDetailsSidePanel } from "./components/RemovalProfileDetailsSidePanel";
 export { default as RemovalProfileEditSidePanel } from "./components/RemovalProfileEditSidePanel";
-export { default as RemovalProfileList } from "./components/RemovalProfileList";
-export { default as RemovalProfilesEmptyState } from "./components/RemovalProfilesEmptyState";
-export { default as RemovalProfilesHeader } from "./components/RemovalProfilesHeader";
-export { useRemovalProfiles } from "./hooks";
+export { default as RemovalProfileContainer } from "./components/RemovalProfileContainer";
 export type { RemovalProfile } from "./types";
