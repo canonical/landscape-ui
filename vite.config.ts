425374b81c36f60a4c4159e15f7f8de47df12b5d
Ethan Shaw <ethan.shaw@canonical.com>
2026-03-11 22:02:34 +0000

    Add Vite plugin for React

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import * as path from "path";
 import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 export default defineConfig(({ mode }) => {
@@ -8,6 +9,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [
+      react(),
       eslint(),
       {
         name: "exclude-msw",
