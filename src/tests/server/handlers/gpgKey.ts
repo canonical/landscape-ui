97fb99f283a96b24235460c9dfd595e18a27393c
Gabe Gaghi <gabe.gaghi@canonical.com>
2026-04-09 17:54:36 -0300

    add gpg mock and test submission

--- src/tests/server/handlers/gpgKey.ts
+++ src/tests/server/handlers/gpgKey.ts
@@ -1,10 +1,29 @@
 import { http, HttpResponse } from "msw";
-import { API_URL_OLD } from "@/constants";
-import type { GetGPGKeysParams, GPGKey } from "@/features/gpg-keys";
+import { API_URL, API_URL_OLD } from "@/constants";
+import type {
+  GetGPGKeysParams,
+  ImportGPGKeyParams,
+  GPGKey,
+} from "@/features/gpg-keys";
 import { gpgKeys } from "@/tests/mocks/gpgKey";
 import { isAction } from "@/tests/server/handlers/_helpers";
+import { getEndpointStatus } from "@/tests/controllers/controller";
 
 export default [
+  http.post<ImportGPGKeyParams, never, GPGKey>(
+    `${API_URL}gpg-key`,
+    async ({ request }) => {
+      const endpointStatus = getEndpointStatus();
+      const { name } = await request.json();
+
+      if (endpointStatus.status === "error") {
+        throw new HttpResponse(null, { status: 500 });
+      }
+
+      return HttpResponse.json<GPGKey>({ ...gpgKeys[0], name });
+    },
+  ),
+
   http.get<never, GetGPGKeysParams, GPGKey[]>(API_URL_OLD, ({ request }) => {
     if (!isAction(request, "GetGPGKeys")) {
       return;
