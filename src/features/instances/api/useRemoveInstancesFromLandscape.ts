d5b455b1828630df013edbf9d4d4c73485839e04
Joey Mucci <joseph.mucci@canonical.com>
2026-04-13 00:59:15 +0000

    choose endpoint based on ff + tests

--- src/features/instances/api/useRemoveInstancesFromLandscape.ts
+++ src/features/instances/api/useRemoveInstancesFromLandscape.ts
@@ -1,4 +1,6 @@
+import useAuth from "@/hooks/useAuth";
 import useFetch from "@/hooks/useFetch";
+import useFetchOld from "@/hooks/useFetchOld";
 import type { ApiError } from "@/types/api/ApiError";
 import type { Instance } from "@/types/Instance";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -8,16 +10,39 @@ export interface RemoveInstancesParams {
   computer_ids: number[];
 }
 
+// Currently determining which endpoint to use with a feature flag
 export const useRemoveInstancesFromLandscape = () => {
-  const authFetch = useFetch();
+  const { isFeatureEnabled } = useAuth();
   const queryClient = useQueryClient();
 
+
+  if (isFeatureEnabled("computer-soft-deletion")) {
+    const authFetch = useFetch();
+    const { isPending, mutateAsync } = useMutation<
+    AxiosResponse<Instance[]>,
+    AxiosError<ApiError>,
+    RemoveInstancesParams
+  >({
+    mutationFn: async (params: RemoveInstancesParams) =>
+      authFetch.post("computers:delete", params),
+    onSuccess: async () =>
+      queryClient.invalidateQueries({ queryKey: ["instances"] }),
+  });
+
+    return {
+      removeInstancesFromLandscape: mutateAsync,
+      isRemovingInstancesFromLandscape: isPending,
+    };
+  }
+
+  const authFetchOld = useFetchOld();
   const { isPending, mutateAsync } = useMutation<
     AxiosResponse<Instance[]>,
     AxiosError<ApiError>,
     RemoveInstancesParams
   >({
-    mutationFn: async (params) => authFetch.post("computers:delete", params),
+    mutationFn: async (params: RemoveInstancesParams) =>
+      authFetchOld.get("RemoveComputers", { params }),
     onSuccess: async () =>
       queryClient.invalidateQueries({ queryKey: ["instances"] }),
   });
