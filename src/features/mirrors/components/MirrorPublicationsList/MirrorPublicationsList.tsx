fb048b03c1c539cf8223dd4e6d42d9cbaf64382f
Ethan Shaw <ethan.shaw@canonical.com>
2026-04-24 17:54:45 +0000

    `RemoveMirrorModal` improvements

--- src/features/mirrors/components/MirrorPublicationsList/MirrorPublicationsList.tsx
+++ src/features/mirrors/components/MirrorPublicationsList/MirrorPublicationsList.tsx
@@ -9,10 +9,12 @@ import { Link } from "react-router";
 
 interface MirrorPublicationsListProps {
   readonly publications: Publication[];
+  readonly openInNewTab?: boolean;
 }
 
 const MirrorPublicationsList: FC<MirrorPublicationsListProps> = ({
   publications,
+  openInNewTab = false,
 }) => {
   const { count, increment, decrement } = useCounter(1);
 
@@ -27,6 +29,7 @@ const MirrorPublicationsList: FC<MirrorPublicationsListProps> = ({
                 pathname: "/repositories/publications",
                 search: `?sidePath=view&name=${publication.publicationId}`,
               }}
+              target={openInNewTab ? "_blank" : undefined}
             >
               {publication.label}
             </Link>
@@ -34,7 +37,7 @@ const MirrorPublicationsList: FC<MirrorPublicationsListProps> = ({
         },
       },
     ],
-    [],
+    [openInNewTab],
   );
 
   return (
