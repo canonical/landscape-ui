825f9da5c35c8152bd8cdceb9b998c5f988ebb5b
Marc Bucchieri <marc.bucchieri@canonical.com>
2026-04-29 08:34:47 -0700

    more profile fixes

--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -1,16 +1,23 @@
-export type ProfileType =
-  | "package"
-  | "reboot"
-  | "removal"
-  | "repository"
-  | "security"
-  | "script"
-  | "upgrade"
-  | "wsl";
+export type ProfileActions =
+  | "edit"
+  | "edit-constraints"
+  | "run"
+  | "download"
+  | "duplicate"
+  | "view";
+
+export interface ComplianceInstanceCounts {
+  constrained: number[];
+  "non-compliant": number[];
+  pending: number[];
+}
 
 export interface Profile extends Record<string, unknown> {
+  access_group: string;
+  all_computers: boolean;
+  description?: string;
   id: number;
-  name: string | null;
+  name: string;
+  tags: string[];
   title: string;
-  type: ProfileType;
 }
