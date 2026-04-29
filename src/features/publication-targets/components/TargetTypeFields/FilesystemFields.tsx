536493b30548563f9f82400046ff64b2a4846429
Yurii Vasyliev <yurii.vasyliev@canonical.com>
2026-04-29 16:25:15 +0200

    refactor: nest AddPublicationTargetForm values by target type

--- src/features/publication-targets/components/TargetTypeFields/FilesystemFields.tsx
+++ src/features/publication-targets/components/TargetTypeFields/FilesystemFields.tsx
@@ -10,23 +10,27 @@ interface FilesystemFieldsProps {
   readonly formik: FormikProps<AddPublicationTargetFormValues>;
 }
 
-const FilesystemFields: FC<FilesystemFieldsProps> = ({ formik }) => (
-  <div className={styles.typeFieldsIndent}>
-    <Input
-      type="text"
-      label="Path"
-      help={!getFormikError(formik, "path") ? "File system path starting with /" : undefined}
-      required
-      error={getFormikError(formik, "path")}
-      {...formik.getFieldProps("path")}
-    />
-    <Select
-      label="Link method"
-      options={LINK_METHOD_OPTIONS}
-      error={getFormikError(formik, "linkMethod")}
-      {...formik.getFieldProps("linkMethod")}
-    />
-  </div>
-);
+const FilesystemFields: FC<FilesystemFieldsProps> = ({ formik }) => {
+  const pathError = getFormikError(formik, ["filesystem", "path"]);
+
+  return (
+    <div className={styles.typeFieldsIndent}>
+      <Input
+        type="text"
+        label="Path"
+        help={!pathError ? "File system path starting with /" : undefined}
+        required
+        error={pathError}
+        {...formik.getFieldProps("filesystem.path")}
+      />
+      <Select
+        label="Link method"
+        options={LINK_METHOD_OPTIONS}
+        error={getFormikError(formik, ["filesystem", "linkMethod"])}
+        {...formik.getFieldProps("filesystem.linkMethod")}
+      />
+    </div>
+  );
+};
 
 export default FilesystemFields;
