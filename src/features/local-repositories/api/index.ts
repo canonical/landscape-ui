551397ec7217e6c2f747b3d0bd566288e9abd0f4
Gabe Esquivel Gaghi <gabe.gaghi@canonical.com>
2026-05-01 15:32:07 -0300

    delete moved api hook

--- src/features/local-repositories/api/index.ts
+++ src/features/local-repositories/api/index.ts
@@ -2,4 +2,3 @@ export * from "./useGetLocalRepositories";
 export * from "./useGetRepositoryPackages";
 export * from "./useRemoveLocalRepository";
 export * from "./useBatchGetLocals";
-export * from "./useGetOperation";
