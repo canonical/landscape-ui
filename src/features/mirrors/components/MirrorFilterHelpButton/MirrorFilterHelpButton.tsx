15c6e67ace72051d9c8f39bbe98e6c21a958f9e7
Ethan Shaw <ethan.shaw@canonical.com>
2026-05-07 20:23:50 +0000

    fix modal

--- src/features/mirrors/components/MirrorFilterHelpButton/MirrorFilterHelpButton.tsx
+++ src/features/mirrors/components/MirrorFilterHelpButton/MirrorFilterHelpButton.tsx
@@ -12,6 +12,7 @@ import { useBoolean } from "usehooks-ts";
 import type { Column } from "react-table";
 import type { Term } from "./types";
 import { PACKAGE_FILTER_HELP_DATA } from "./constants";
+import { createPortal } from "react-dom";
 
 const MirrorFilterHelpButton: FC = () => {
   const {
@@ -47,29 +48,34 @@ const MirrorFilterHelpButton: FC = () => {
       >
         <Icon name={ICONS.help} />
       </Button>
-      {isModalOpen && (
-        <Modal
-          title="Package query syntax"
-          close={closeModal}
-          className={classes.modal}
-        >
-          <p>
-            Available search terms for use in package queries. If multiple terms
-            are separated by <code>|</code> (OR), any of the conditions will
-            match. Terms separated by <code>,</code> (AND) must all match. When
-            a term is preceded by <code>!</code> (NOT), the condition must not
-            match. Use parentheses <code>()</code> to group terms. Values
-            containing spaces or ambiguous characters must be quoted with{" "}
-            <code>&apos;</code> or <code>&quot;</code> (escape inner quotes with{" "}
-            <code>\</code>).
-          </p>
-          <ModularTable
-            className="u-no-margin--bottom"
-            columns={columns}
-            data={PACKAGE_FILTER_HELP_DATA}
-          />
-        </Modal>
-      )}
+      {isModalOpen &&
+        createPortal(
+          <Modal
+            title="Package query syntax"
+            close={closeModal}
+            className={classes.modal}
+          >
+            <p>
+              Available search terms for use in package queries. If multiple
+              terms are separated by <code>|</code> (OR), any of the conditions
+              will match. Terms separated by <code>,</code> (AND) must all
+              match. When a term is preceded by <code>!</code> (NOT), the
+              condition must not match. Use parentheses <code>()</code> to group
+              terms. Values containing spaces or ambiguous characters must be
+              quoted with <code>&apos;</code> or <code>&quot;</code> (escape
+              inner quotes with <code>\</code>).
+            </p>
+            <div className={classes.tableWrapper}>
+              <ModularTable
+                className="u-no-margin--bottom"
+                columns={columns}
+                data={PACKAGE_FILTER_HELP_DATA}
+                style={{ minWidth: "60rem" }}
+              />
+            </div>
+          </Modal>,
+          document.body,
+        )}
     </>
   );
 };
