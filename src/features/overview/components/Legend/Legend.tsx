6bcc40d6a35b76d1d2f4acfc816d53610fda4d64
Yurii Vasyliev <yurii.vasyliev@canonical.com>
2026-05-11 17:14:38 +0200

    fix: add pluralize helper

--- src/features/overview/components/Legend/Legend.tsx
+++ src/features/overview/components/Legend/Legend.tsx
@@ -3,6 +3,7 @@ import type { FC } from "react";
 import { Link } from "react-router";
 import { ALERT_STATUSES } from "@/features/instances";
 import { ROUTES } from "@/libs/routes";
+import { pluralizeWithCount } from "@/utils/_helpers";
 import classes from "./Legend.module.scss";
 
 export interface LegendItem {
@@ -72,14 +73,14 @@ const Legend: FC<LegendProps> = ({
             </div>
             {item.count === 0 ? (
               <span className="u-no-margin u-no-padding">
-                {item.count} instances
+                {pluralizeWithCount(item.count, "instance")}
               </span>
             ) : (
               <Link
                 to={ROUTES.instances.root({ status: statusItem.filterValue })}
                 className="u-no-margin u-no-padding"
               >
-                {item.count} instances
+                {pluralizeWithCount(item.count, "instance")}
               </Link>
             )}
           </div>
