519f87bf2aa6afb865745bbbe5c90e18aab904c4
Ethan Shaw <ethan.shaw@canonical.com>
2026-05-28 19:04:49 +0000

    minor refactor

--- src/components/layout/EmptyState.tsx
+++ src/components/layout/EmptyState.tsx
@@ -1,5 +1,6 @@
 import type { FC, ReactNode } from "react";
 import classes from "./EmptyState.module.scss";
+import classNames from "classnames";
 
 interface EmptyStateProps {
   readonly title?: string;
@@ -7,7 +8,7 @@ interface EmptyStateProps {
   readonly link?: { href: string; text: string };
   readonly icon?: string;
   readonly cta?: ReactNode[];
-  readonly size?: "fixed" | "max";
+  readonly className?: string;
 }
 
 const EmptyState: FC<EmptyStateProps> = ({
@@ -16,33 +17,27 @@ const EmptyState: FC<EmptyStateProps> = ({
   link,
   icon = "",
   cta = [],
-  size = "fixed",
+  className,
 }) => {
   return (
-    <div className="p-strip">
-      <div className={`${classes.inner} size-${size}`}>
-        {icon && (
-          <div>
-            <span
-              style={{ width: 36, height: 36, opacity: 0.2 }}
-              className={`p-icon--${icon}`}
-              aria-hidden
-            />
-          </div>
-        )}
-        {title && <p className="p-heading--4 u-no-margin">{title}</p>}
-        {body && <div className={classes.body}>{body}</div>}
-        {link && (
-          <a
-            href={link.href}
-            target="_blank"
-            rel="nofollow noopener noreferrer"
-          >
-            {link.text}
-          </a>
-        )}
-        {cta.length > 0 && <div className={classes.cta}>{cta}</div>}
-      </div>
+    <div className={classNames("p-strip", classes.inner, className)}>
+      {icon && (
+        <div>
+          <span
+            style={{ width: 36, height: 36, opacity: 0.2 }}
+            className={`p-icon--${icon}`}
+            aria-hidden
+          />
+        </div>
+      )}
+      {title && <p className="p-heading--4 u-no-margin">{title}</p>}
+      {body && <div className={classes.body}>{body}</div>}
+      {link && (
+        <a href={link.href} target="_blank" rel="nofollow noopener noreferrer">
+          {link.text}
+        </a>
+      )}
+      {cta.length > 0 && <div className={classes.cta}>{cta}</div>}
     </div>
   );
 };
