fb02a89bac8345c5d7da57614714b27141c9d220
Ethan Shaw <ethan.shaw@canonical.com>
2026-06-01 16:18:33 +0000

    Move tooltip out of label

--- src/components/form/CheckboxInputWithHelp/CheckboxInputWithHelp.tsx
+++ src/components/form/CheckboxInputWithHelp/CheckboxInputWithHelp.tsx
@@ -9,26 +9,31 @@ interface CheckboxInputWithHelpProps extends InputProps {
 const CheckboxInputWithHelp = ({
   label,
   tooltipMessage,
+  disabled,
   ...checkboxInputProps
 }: CheckboxInputWithHelpProps) => {
   return (
-    <Input
-      type="checkbox"
-      label={
-        <span>
-          <span className={classes.settingLabel}>{label}</span>
-          <Tooltip
-            message={tooltipMessage}
-            position="top-center"
-            positionElementClassName={classes.tooltipPositionElement}
-          >
-            <Icon name={ICONS.help} aria-hidden />
-            <span className="u-off-screen">Help</span>
-          </Tooltip>
-        </span>
-      }
-      {...checkboxInputProps}
-    />
+    <div className={classes.container}>
+      <Input
+        type="checkbox"
+        label={label}
+        disabled={disabled}
+        {...checkboxInputProps}
+      />
+      <Tooltip
+        message={tooltipMessage}
+        position="top-center"
+        className={classes.tooltip}
+        positionElementClassName={classes.tooltipPositionElement}
+      >
+        <Icon
+          name={ICONS.help}
+          aria-hidden
+          className={disabled ? classes.disabled : undefined}
+        />
+        <span className="u-off-screen">Help</span>
+      </Tooltip>
+    </div>
   );
 };
 
