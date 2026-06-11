260ff67e59469678354e7ced988844c248b530fc
Ethan Shaw <ethan.shaw@canonical.com>
2026-06-11 22:01:34 +0000

    Finish redirection state

--- src/hooks/useRedirectToClassicDashboard.ts
+++ src/hooks/useRedirectToClassicDashboard.ts
@@ -1,21 +1,31 @@
 import { useBoolean } from "usehooks-ts";
 import useFetch from "./useFetch";
 import { redirectToExternalUrl } from "@/features/auth";
+import useDebug from "./useDebug";
 
 export const useRedirectToClassicDashboard = (path = "") => {
+  const debug = useDebug();
   const authFetch = useFetch();
 
-  const { value: isRedirecting, setTrue: setIsRedirectingTrue } = useBoolean();
+  const {
+    value: isRedirecting,
+    setTrue: setIsRedirectingTrue,
+    setFalse: setIsRedirectingFalse,
+  } = useBoolean();
 
   return {
     redirectToClassicDashboard: async () => {
-      setIsRedirectingTrue();
-
-      const response = await authFetch.get<{ url: string }>(
-        "classic_dashboard_url",
-      );
-
-      redirectToExternalUrl(response.data.url + path);
+      try {
+        setIsRedirectingTrue();
+        const response = await authFetch.get<{ url: string }>(
+          "classic_dashboard_url",
+        );
+        redirectToExternalUrl(response.data.url + path);
+      } catch (error) {
+        debug(error);
+      } finally {
+        setIsRedirectingFalse();
+      }
     },
 
     isRedirectingToClassicDashboard: isRedirecting,
