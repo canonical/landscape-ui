99b35ccabd626a6d3cd185e2db0e067ef060d106
Gabe Esquivel Gaghi <gabe.gaghi@canonical.com>
2026-07-07 13:19:07 -0300

    fix test

--- src/components/ui/ProgressBar/ProgressBar.test.tsx
+++ src/components/ui/ProgressBar/ProgressBar.test.tsx
@@ -5,9 +5,7 @@ import ProgressBar from "./ProgressBar";
 
 describe("ProgressBar", () => {
   it("sets the inner bar width to the given percentage", () => {
-    const { container } = renderWithProviders(
-      <ProgressBar progressPercent={75} />,
-    );
+    const { container } = renderWithProviders(<ProgressBar progress={75} />);
 
     const innerBar = container.querySelector("[style]");
     expect(innerBar).toHaveStyle({ width: "75%" });
