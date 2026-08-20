a49cfd798ab2c84d069a12b83cea70487131fd90
Ethan Shaw <ethan.shaw@canonical.com>
2026-08-20 06:19:30 +0000

    build: fix Vite build warnings [LNDENG-4163] (#751)
    Fixed the three different types of warnings we were getting on `vite
    build`:
    1. Some components were intended to be lazy-loaded, but were imported
    directly in some file, causing them to be statically imported.
    2. We have one very large chunk due to the Monaco editor. The editor is
    already lazy-loaded, so that chunk is as small as it can be. I raised
    the chunk size limit since it is not a meaningful warning to us, but we
    can remove the limit when we move off of Monaco.
    3. Our project overrides the asset path for Vanilla, but we didn't
    include all of the assets locally. Even though we don't use all of them,
    we should still include them in the project.

--- src/features/upgrade-profiles/index.ts
+++ src/features/upgrade-profiles/index.ts
@@ -1,6 +1,4 @@
 export { useGetPageUpgradeProfile, useRemoveUpgradeProfile } from "./api";
 export { useUpgradeProfiles } from "./hooks";
-export { default as UpgradeProfileAddSidePanel } from "./components/UpgradeProfileAddSidePanel";
-export { default as UpgradeProfileEditSidePanel } from "./components/UpgradeProfileEditSidePanel";
 export { default as ViewUpgradeProfileDetailsBlock } from "./components/ViewUpgradeProfileDetailsBlock";
 export type { UpgradeProfile } from "./types";
