d9147ffce89ceb411962b43919ef2ea7726c125e
Ryan <128562343+RyanLoi98@users.noreply.github.com>
2026-09-01 17:50:12 -0600

    feat: added form section and some description text and the button

--- src/pages/dashboard/account/self-hosted-license/SelfHostedLicensePage.tsx
+++ src/pages/dashboard/account/self-hosted-license/SelfHostedLicensePage.tsx
@@ -1,7 +1,8 @@
 import PageContent from "@/components/layout/PageContent";
 import PageHeader from "@/components/layout/PageHeader";
 import PageMain from "@/components/layout/PageMain";
-import { Link } from "@canonical/react-components";
+import FormSection from "@/components/form/FormSection";
+import { Button, Link } from "@canonical/react-components";
 import type { FC } from "react";
 import { SELF_HOSTED_LANDSCAPE_DOCUMENTATION_URL } from "./constants";
 import classes from "./SelfHostedLicensePage.module.scss";
@@ -26,9 +27,32 @@ const SelfHostedLicensePage: FC = () => {
           </p>
         }
       />
-      <PageContent>
+      <PageContent container="medium" align="left">
         <hr className="p-rule" />
-        
+        <FormSection title="Setting up the license file">
+          <p>
+            In order to run Self-hosted Landscape, a license file needs to be
+            present on the system where the software will be installed.
+          </p>
+
+          <p>
+            Your license file contains information that uniquely indentifies
+            your account and the number of seats you are entitled to, as well as
+            the expiration date of your license.
+          </p>
+
+          <Button appearance="positive" hasIcon>
+            <i className="p-icon--begin-downloading" />
+            <span>Download license file</span>
+          </Button>
+
+          <p>
+            Self-hosted Landscape expects to find your license file in the
+            following location:
+          </p>
+
+          
+        </FormSection>
       </PageContent>
     </PageMain>
   );
