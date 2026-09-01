d10dfe17c94178378c1b1aa4ff2fb324803805a3
Ryan <128562343+RyanLoi98@users.noreply.github.com>
2026-09-01 13:05:01 -0600

    feat: added page description

--- src/pages/dashboard/account/self-hosted-license/SelfHostedLicensePage.tsx
+++ src/pages/dashboard/account/self-hosted-license/SelfHostedLicensePage.tsx
@@ -1,11 +1,28 @@
+import PageContent from "@/components/layout/PageContent";
 import PageHeader from "@/components/layout/PageHeader";
 import PageMain from "@/components/layout/PageMain";
+import { Link } from "@canonical/react-components";
 import type { FC } from "react";
+import { SELF_HOSTED_LANDSCAPE_DOCUMENTATION_URL } from "./constants";
+import classes from "./SelfHostedLicensePage.module.scss";
 
 const SelfHostedLicensePage: FC = () => {
   return (
     <PageMain>
       <PageHeader title="Self hosted license" />
+      <PageContent>
+        <p className={`${classes.description} p-text--small u-text--muted`}>
+          Self-hosted Landscape is the standalone version of Landscape that you
+          can install on-premises or in a public cloud.{" "}
+          <Link
+            href={SELF_HOSTED_LANDSCAPE_DOCUMENTATION_URL}
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+          >
+            Learn more about self hosted landscape
+          </Link>
+        </p>
+      </PageContent>
     </PageMain>
   );
 };
---
+++ src/pages/dashboard/account/self-hosted-license/constants.ts
@@ -0,0 +1,7 @@
+import { EXTERNAL_PATHS } from "@/libs/routes/external";
+
+/**
+ * Documentation URL for this page.
+ * Build from EXTERNAL_PATHS.documentation to keep domain/base-path changes centralized.
+ */
+export const SELF_HOSTED_LANDSCAPE_DOCUMENTATION_URL = `${EXTERNAL_PATHS.documentation}/explanation/landscape/self-hosted-landscape/`;
