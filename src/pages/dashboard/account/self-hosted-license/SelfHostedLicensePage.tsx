49acbf7e07d35dc30a94afdadfc973f5b9209bec
Ryan <128562343+RyanLoi98@users.noreply.github.com>
2026-09-04 13:04:18 -0600

    feat: made the download license file button open in a new tab instead of replacing the current tab

--- src/pages/dashboard/account/self-hosted-license/SelfHostedLicensePage.tsx
+++ src/pages/dashboard/account/self-hosted-license/SelfHostedLicensePage.tsx
@@ -7,7 +7,6 @@ import { ROUTES } from "@/libs/routes";
 import { Button, CodeSnippet, Link } from "@canonical/react-components";
 import type { FC } from "react";
 import { Link as RouterLink } from "react-router";
-import { redirectToExternalUrl } from "@/features/auth";
 import { SELF_HOSTED_LANDSCAPE_DOCUMENTATION_URL } from "./constants";
 import { useGetSelfHostedLicense } from "./api/useGetSelfHostedLicense";
 import classes from "./SelfHostedLicensePage.module.scss";
@@ -25,7 +24,7 @@ ${downloadUrl}`
 
   const handleDownload = () => {
     if (downloadUrl) {
-      redirectToExternalUrl(downloadUrl);
+      window.open(downloadUrl, "_blank", "noopener,noreferrer");
     }
   };
 
